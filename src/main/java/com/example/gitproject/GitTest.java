1f199398cba2c3ef0bb81dfaac78dc5c6185ab64
youxianbin <cnalbin@gmail.com>
2023-01-30 10:15:07 +0800

    commit 2

--- src/main/java/com/example/gitproject/GitTest.java
+++ src/main/java/com/example/gitproject/GitTest.java
@@ -3,5 +3,7 @@ package com.example.gitproject;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("ddddddddddwwwww");
+
+        System.out.println("xxxx");
     }
 }
